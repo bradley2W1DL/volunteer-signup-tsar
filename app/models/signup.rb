ecea90742f42bc4aae3b9065a8101cb404ea2fa2
Bradley Berger <bradley.s.berger@gmail.com>
2016-03-29 23:53:24 -0600

    add validations to signup model that require email, name, and phone number

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,8 @@
 class Signup < ActiveRecord::Base
   belongs_to :shift
   has_many :messages
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
